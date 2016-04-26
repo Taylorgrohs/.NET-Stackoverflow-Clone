a439a3014be3f4c2509d4ee896a5e9a78240de68
Taylor <taylorgrohs@gmail.com>
2016-04-26 11:48:09 -0700

    add delete role controller

--- StackClone/src/StackClone/Controllers/RolesController.cs
+++ StackClone/src/StackClone/Controllers/RolesController.cs
@@ -49,5 +49,20 @@ namespace StackClone.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        // GET: /Roles/Delete
+        public IActionResult Delete( string RoleName)
+        {
+            var thisRole = _db.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            _db.Roles.Remove(thisRole);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: /Roles/Edit
+        public IActionResult Edit(string RoleName)
+        {
+            var thisRole = _db.Roles.Where(r = r.Name.Equals(RoleName, ))
+        }
     }
 }
